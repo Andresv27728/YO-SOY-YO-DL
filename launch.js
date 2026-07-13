6af8dda49674a6ca6fa526dd9585060451ef0f17
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2026-07-13 18:27:29 -0500

    fix: use direct electron.exe path to avoid EINVAL

--- launch.js
+++ launch.js
@@ -1,6 +1,6 @@
 const{spawn}=require("child_process");
 const path=require("path");
-const ep=path.join(__dirname,"node_modules",".bin","electron.cmd");
+const ep=path.join(__dirname,"node_modules","electron","dist","electron.exe");
 const app=path.join(__dirname,"main.js");
-const p=spawn(ep,[app],{detached:true,stdio:"ignore",windowsHide:true});
+const p=spawn(ep,[app],{detached:true,stdio:"ignore",windowsHide:true,cwd:__dirname});
 p.unref();
