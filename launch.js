1a9f2386b71da1e52f5d44f9be776c8ceb2eda14
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2026-07-13 19:20:06 -0500

    revert: keep console visible, remove launch.js

--- launch.js
+++
@@ -1,6 +0,0 @@
-const{spawn}=require("child_process");
-const path=require("path");
-const ep=path.join(__dirname,"node_modules","electron","dist","electron.exe");
-const app=path.join(__dirname,"main.js");
-const p=spawn(ep,[app],{detached:true,stdio:"ignore",windowsHide:true,cwd:__dirname});
-p.unref();
--- start-hidden.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" wscript "%~dp0YO SOY YO DL.vbs"
